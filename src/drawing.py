2766742f4a48afc8b85435a6c39d38fcc4a5aa0d
Kieran McKenna <kieranm@pdx.edu>
2026-02-16 11:31:05 -0800

    Window title removed

--- src/drawing.py
+++ src/drawing.py
@@ -18,8 +18,6 @@ def _add_point(trail, x, y, draw_from_prev):
 
 def run():
 	pygame.init()
-	pygame.display.set_caption("Touch Trail")
-
 	screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 	width, height = screen.get_size()
 
